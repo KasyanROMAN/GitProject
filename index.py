a7404589b15eee036f9379597137e9fd6b926631
Kasyan Roman <kasanr223@gmail.com>
2024-11-15 17:34:55 +0200

    lllll

--- index.py
+++ index.py
@@ -5,3 +5,21 @@ print('Good')
 
 for i in range(10):
     print(i)
+
+
+print('Hello')
+print(111111111111111111111)
+
+print('Good')
+
+for i in range(10):
+    print(i)
+
+
+print('Hello')
+print(111111111111111111111)
+
+print('Good')
+
+for i in range(10):
+    print(i)
