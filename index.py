e45c33860e1d2e35a49a94d5b7911a8065ddb4c4
Kiril Yaremenko <kiril7yaremenko@gmail.com>
2024-11-15 16:39:06 +0100

    super

--- index.py
+++ index.py
@@ -1,3 +1,22 @@
 print('Hello')
 
 print(123+1)
+
+print('Hello')
+
+print(123+1)
+
+print('Hello')
+
+print(123+1)
+
+print('Hello')
+
+print(123+1)
+
+print('Hello')
+
+print(123+1)
+
+for i in range(99):
+    print(i+11)
